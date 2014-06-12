629a9c39a4611933d2e0215d78b116b7046751b2
Szabolcs Balogh <baloghsz@szabi.org>
2014-06-12 23:24:45 +0200

    paginator moved

--- _assets/javascripts/sajto.js
+++ _assets/javascripts/sajto.js
@@ -1,20 +1,12 @@
-if ($("#sajto-hirek-1").length > 0) {
+if ($("#sajto-hirek").length > 0) {
   var sajto_data = {};
   var sajto_data_rows = 0;
   var sajto_page_size = 20;
 
   function sajto_page(page_active) {
-    $("#sajto-hirek-2").empty();
+    $("#sajto-hirek").empty();
     for(var i=(page_active-1)*sajto_page_size+2; i < (page_active)*sajto_page_size+2 && i<=sajto_data_rows; i++)
-        $("#sajto-hirek-2").append('<li><strong>'+sajto_data[i]['B']+'</strong>: <a href="'+sajto_data[i]['E']+'" target="_blank">'+sajto_data[i]['D']+'</a></li>');
-
-    $("#sajto-hirek-2").slideToggle(1200);
-    $("#sajto-hirek-1").slideToggle(1200, function() {
-        $("#sajto-hirek-1").empty();
-        $("#sajto-hirek-1").append($("#sajto-hirek-2").html());
-        $("#sajto-hirek-2").toggle();
-        $("#sajto-hirek-1").toggle();
-    });
+        $("#sajto-hirek").append('<li><strong>'+sajto_data[i]['B']+'</strong>: <a href="'+sajto_data[i]['E']+'" target="_blank">'+sajto_data[i]['D']+'</a></li>');
 
     $("#sajto-pages").empty();
     for(var i=1; i < sajto_data_rows/sajto_page_size+1; i++)
@@ -30,7 +22,6 @@ if ($("#sajto-hirek-1").length > 0) {
         sajto_data[parseInt(e.title.$t.replace(/[A-Z]+/, ''))][e.title.$t.replace(/[0-9]+/, '')] = e.content.$t;
     });
 
-    $("#sajto-hirek-2").hide();
     sajto_page(1);
   });
 
